ebfe5f31d8db2721f941ffe5548e50f6a94e6ff9
grote-florian <florian-grote@hotmail.com>
2026-02-19 16:16:43 +0100

    Create NetworkModule.kt for dependency injection using Dagger and Retrofit

---
+++ app/src/main/java/com/example/arcanevaluecompanion/di/NetworkModule.kt
@@ -0,0 +1,18 @@
+import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+class NetworkModule {
+
+    @Provides
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.example.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    // Add other network-related provides here
+}
