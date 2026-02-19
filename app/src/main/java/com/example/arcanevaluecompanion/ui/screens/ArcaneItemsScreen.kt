cc0e57e8f4a8e59cce2a865b4f6e30b9e12bf7ec
grote-florian <florian-grote@hotmail.com>
2026-02-19 16:17:08 +0100

    Create main screen file ArcaneItemsScreen.kt

---
+++ app/src/main/java/com/example/arcanevaluecompanion/ui/screens/ArcaneItemsScreen.kt
@@ -0,0 +1,9 @@
+package com.example.arcanevaluecompanion.ui.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ArcaneItemsScreen() {
+    Text(text = "Arcane Items")
+}
