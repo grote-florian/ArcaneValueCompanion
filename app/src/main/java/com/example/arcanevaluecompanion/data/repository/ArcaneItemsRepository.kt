c1b7602e4addbc02bccfe478755b92326c45862b
grote-florian <florian-grote@hotmail.com>
2026-02-19 16:16:35 +0100

    Create ArcaneItemsRepository.kt

---
+++ app/src/main/java/com/example/arcanevaluecompanion/data/repository/ArcaneItemsRepository.kt
@@ -0,0 +1,9 @@
+// ArcaneItemsRepository.kt
+
+package com.example.arcanevaluecompanion.data.repository
+
+// Add your code and logic for the ArcaneItemsRepository here
+
+class ArcaneItemsRepository {
+    // Define repository methods and data management for arcane items
+}
